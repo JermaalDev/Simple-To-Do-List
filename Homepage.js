178f494d8f58159c7e33118629d46fcae4d3cb87
JermaalDev <jermaal@bluestone.africa>
2025-02-05 15:57:40 +0200

    feat: created the html interface

---
+++ Homepage.js
@@ -0,0 +1,26 @@
+
+const taskInput = document.getElementById("input-field");
+const addTaskButton = document.getElementById("add-button");
+const taskList = document.getElementById("task-list");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
